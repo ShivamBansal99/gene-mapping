db3123d47f9f490e046d77e55a0c0b7e67c6493a
Shivam Bansal <32170952+ShivamBansal99@users.noreply.github.com>
2019-08-21 11:59:03 +0530

    adds rand_init and cost

--- local_search.cpp
+++ local_search.cpp
@@ -1,6 +1,39 @@
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
+float cost(vector<int> str[],vector<vector<float> > MC,float CC,int K,int V){
+	  int max=0;
+	  for(int i=0;i<K;i++){
+		    for(int j=0;j<str[i].size();j++){
+		      	if(str[i][j]!=V && max<j) max=j;
+		    }
+	  }
+	  float cost1=0;
+	  for(int i=0;i<K;i++){
+		    for(int j=i+1;j<K;j++){
+			      for(int k=0;k<=max;k++){
+					cost1+=MC[str[i][k]][str[j][k]];
+			      }
+		    }
+	  	    for(int k=0;k<=max;k++){
+			if(str[i][k]==V) cost1+=CC;
+		    }
+	  }
+	  return cost1;
+}
+void random_init(vector<int> str[],int K,int V){
+	int max=0;
+	for(int i=0;i<k;i++){
+		max+=str[i].size();
+	}
+	for(int i=0;i<k;i++){
+		while(str[i].size()<max){
+			str[i].push_back(V);
+		}
+		random_shuffle(str[i].begin(), str[i].end());
+	}
+	return;
+}
 int main(int argc,char* argv[]){
 	if(argc<3) return 0;
 	ifstream infile;
@@ -14,44 +47,24 @@ int main(int argc,char* argv[]){
 	infile>>time;
 	infile>>V;
 	string s;
-	//infile>>s;
 	infile>>s;
 	
 	char vocab[V];
 	//TO-DO : CONVERT STRING TO ARRAY
-	int j=0;
-	for(int i=0;i<s.length();i++){
-	  vocab[j]=s[i];
-	  j++;
-	  i+=3;
-	}
-	map<char,int> m;
-	for(int i=0;i<V;i++){
-	  m.insert(pair<char,int>(vocab[i],i));
-	}
+	
 	infile>>K;
 	
 	string genes[K];
-	//infile>>s;
 	for(int i=0;i<K;i++){
 		infile>>genes[i];
 	}
-	vector<int> arr[K];
-	for(int i=0;i<K;i++){
-	  for(int j=0;j<genes[i].length();j++){
-	    arr[i].push_back(m.get(genes[i][j]));
-	  }
-	}
 	infile>>CC;
-	vector<vector<float>> MC;
-	float f;
+	float MC[V+1][V+1];
 	for(int i=0;i<=V;i++){
 		for(int j=0;j<=V;j++){
-		  infile>>f;
-		  MC[i].push_back(f);
+			infile>>MC[i][j];
 		}
 	}
-	//infile>>s;
 	infile>>s;
 	if(s!="#"){
 		printf("Corrupt input file...");
