861fdbaa4a977b9e2f1bce1b7ce9036e728a0c0b
shivam-goyal027 <32204630+shivam-goyal027@users.noreply.github.com>
2019-08-21 11:58:11 +0530

    Add files via upload
    mapped string values

--- local_search.cpp
+++ local_search.cpp
@@ -14,27 +14,44 @@ int main(int argc,char* argv[]){
 	infile>>time;
 	infile>>V;
 	string s;
-	infile>>s;
+	//infile>>s;
 	infile>>s;
 	
 	char vocab[V];
 	//TO-DO : CONVERT STRING TO ARRAY
-	
+	int j=0;
+	for(int i=0;i<s.length();i++){
+	  vocab[j]=s[i];
+	  j++;
+	  i+=3;
+	}
+	map<char,int> m;
+	for(int i=0;i<V;i++){
+	  m.insert(pair<char,int>(vocab[i],i));
+	}
 	infile>>K;
 	
 	string genes[K];
-	infile>>s;
+	//infile>>s;
 	for(int i=0;i<K;i++){
 		infile>>genes[i];
 	}
+	vector<int> arr[K];
+	for(int i=0;i<K;i++){
+	  for(int j=0;j<genes[i].length();j++){
+	    arr[i].push_back(m.get(genes[i][j]));
+	  }
+	}
 	infile>>CC;
-	float MC[V+1][V+1];
+	vector<vector<float>> MC;
+	float f;
 	for(int i=0;i<=V;i++){
 		for(int j=0;j<=V;j++){
-			infile>>MC[i][j];
+		  infile>>f;
+		  MC[i].push_back(f);
 		}
 	}
-	infile>>s;
+	//infile>>s;
 	infile>>s;
 	if(s!="#"){
 		printf("Corrupt input file...");
